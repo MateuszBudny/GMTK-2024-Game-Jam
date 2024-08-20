54cec2311823cfa299e976981c8616b12325abe8
Jaruzej <Jaruzej@gmail.com>
2024-08-20 19:49:41 +0200

    Fixed invisble floating pieces

--- Assets/_GMTK/CarpetDrawing/Scripts/Piece.cs
+++ Assets/_GMTK/CarpetDrawing/Scripts/Piece.cs
@@ -1,58 +1,58 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    
+
     [SerializeField] Pattern pattern;
     [SerializeField] PatternGrid patternGrid;
 
-    
+
     private Collider2D roughCollider;
     private Dictionary<Vector2Int, GameObject> pieceObjects = new Dictionary<Vector2Int, GameObject>();
 
     private Transform oldParent;
-    
+
     void Start()
     {
-        if (pattern == null)
+        if(pattern == null)
         {
             Debug.LogError("Piece must have a pattern");
             return;
         }
-        
-        
+
+
         pieceObjects = Pattern.SpawnPattern(pattern.pattern, pattern.singleBlock, transform.position, transform);
 
         RectTransform parentRect = transform.parent?.GetComponent<RectTransform>();
-        if (parentRect != null)
+        if(parentRect != null)
         {
             parentRect.sizeDelta = new Vector2(pattern.size.x, pattern.size.y) * 200;
-            
+
             Vector3[] corners = new Vector3[4];
             parentRect.GetWorldCorners(corners);
-            this.transform.position = corners[0];
+            transform.position = corners[0];
         }
 
         roughCollider = GetComponent<Collider2D>();
-        
+
     }
 
 
     public void StartDrag(Vector3 position)
     {
-        if (transform.parent != null)
+        if(transform.parent != null)
         {
             Piece piece = Instantiate(gameObject, transform.parent).GetComponent<Piece>();
-            foreach (Transform child in piece.transform) {
+            foreach(Transform child in piece.transform)
+            {
                 Destroy(child.gameObject);
             }
-            
+
             piece.transform.parent = null;
             piece.transform.localScale *= pattern.singleBlock.transform.localScale.x / piece.transform.lossyScale.x;
-            StartCoroutine(piece.FollowMouse((position - transform.position) * 0.14f/piece.transform.localScale.x));
+            StartCoroutine(piece.FollowMouse((position - transform.position) * 0.14f / piece.transform.localScale.x));
         }
         else
         {
@@ -63,13 +63,13 @@ public class Piece : MonoBehaviour
 
     public IEnumerator FollowMouse(Vector3 delta)
     {
-        float currentY = transform.position.y;
-        while (true)
+        float currentY = 20;
+        while(true)
         {
             Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - delta;
             newPosition.y = currentY;
             transform.position = newPosition;
-            if (Input.GetMouseButtonUp(0))
+            if(Input.GetMouseButtonUp(0))
             {
                 patternGrid.AddPattern(transform.position, pattern);
                 Destroy(gameObject);
@@ -78,9 +78,9 @@ public class Piece : MonoBehaviour
             yield return null;
         }
     }
-    
+
     public void Update()
     {
-        
+
     }
 }
