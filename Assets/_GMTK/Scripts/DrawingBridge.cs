77ea67a095bd13229cb3fc27a477b4039a63d1b9
Mateusz Budny <mateusz.budny97@gmail.com>
2024-08-20 08:45:52 +0200

    CursorManager added. Changing cursor mode on start and end of carpet drawing added.

--- Assets/_GMTK/Scripts/DrawingBridge.cs
+++ Assets/_GMTK/Scripts/DrawingBridge.cs
@@ -1,19 +1,19 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DrawingBridge : SingleBehaviour<DrawingBridge>
 {
-    
+
     //Drawing carpet Inputs
     public Pattern startPattern;
-    public Pattern wantedPattern; 
+    public Pattern wantedPattern;
     public List<Pattern> usablePatterns = new List<Pattern>();
     public int wallet;
     public CarpetRepairStation carpetRepairStation;
-    
+
     public void EndDrawing(int carpetCost)
     {
+        CursorManager.Instance.CurrentCursorLockMode = CursorLockMode.Locked;
         carpetRepairStation.Exit(carpetCost);
     }
 }
